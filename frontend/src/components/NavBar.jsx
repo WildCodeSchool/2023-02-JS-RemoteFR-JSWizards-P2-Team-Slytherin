b1d3afb37471fde684dd49181fd345d748dfd04f
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-04-18 17:56:45 +0200

    CSS navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,31 +1,27 @@
 const NavBar = () => {
   return (
-    <nav>
-      <img
-        className="logo-hp"
-        src="src/assets/logo/logo-desktop.png"
-        alt="Return to homepage"
-      />
-      <img
-        className="logo-rules"
-        src="src/assets/icon/rules.svg"
-        alt="Game rules"
-      />
-      <img
-        className="logo-score"
-        src="src/assets/icon/score.svg"
-        alt="Scores"
-      />
-      <img
-        className="logo-sound"
-        src="src/assets/icon/sound-on.svg"
-        alt="Sound on"
-      />
-      <img
-        className="logo-settings"
-        src="src/assets/icon/settings.svg"
-        alt="Settings"
-      />
+    <nav id="navbar" class="flex justify-between items-center">
+      <div>
+        <img
+          className="logo-hp"
+          src="src/assets/logo/logo-desktop.png"
+          alt="Return to homepage"
+        />
+      </div>
+      <ul id="menu" class="flex">
+        <li>
+          <img src="src/assets/icon/rules.svg" alt="Game rules" />
+        </li>
+        <li>
+          <img src="src/assets/icon/score.svg" alt="Scores" />
+        </li>
+        <li>
+          <img src="src/assets/icon/sound-on.svg" alt="Sound on" />
+        </li>
+        <li>
+          <img src="src/assets/icon/settings.svg" alt="Settings" />
+        </li>
+      </ul>
     </nav>
   );
 };
