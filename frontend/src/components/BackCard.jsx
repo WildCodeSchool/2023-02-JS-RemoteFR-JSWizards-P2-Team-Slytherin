ebba2eb8504ece078c5998b62b296f3f989604d2
BigBull69 <[slave.69@hotmail.fr]>
2023-04-18 19:37:37 +0200

    completed card component mobil version

--- frontend/src/components/BackCard.jsx
+++ frontend/src/components/BackCard.jsx
@@ -1,5 +1,5 @@
 export default function BackCard() {
   return (
-    <div className="relative flex h-[160px] w-[112px] rounded-[6px] bg-[url('src/assets/img/card-back-background.png')] bg-cover py-2" />
+    <div className="flex h-[88px] w-[60px] rounded-[4px] bg-[url('src/assets/img/card-back-background.png')] bg-cover 2xl:h-[160px] 2xl:w-[112px] 2xl:rounded-[6px]" />
   );
 }
--- frontend/src/components/FrontCard.jsx
+++ frontend/src/components/FrontCard.jsx
@@ -6,23 +6,23 @@ export default function FrontCard({ name, image }) {
   const nameShort = `${firstName}. ${lastName}`;
 
   return (
-    <div className="relative flex h-[160px] w-[112px] flex-col items-center gap-2 rounded-[6px] bg-[url('src/assets/img/card-front-background.png')] bg-cover py-2">
-      <span className="font-baskerville text-sm">{nameShort}</span>
+    <div className="relative flex h-[88px] w-[60px] flex-col items-center rounded-[4px] bg-[url('src/assets/img/card-front-background.png')] bg-cover py-0.5 2xl:h-[160px] 2xl:w-[112px] 2xl:rounded-[6px] 2xl:py-2">
+      <span className="font-baskerville text-2xs 2xl:text-sm">{nameShort}</span>
       <img
         src={image}
         alt={nameShort}
-        className="absolute top-1/2 h-[76px] w-[61px] -translate-y-2/4 rounded-full object-cover"
+        className="absolute top-1/2 h-[42px] w-[33px] -translate-y-2/4 rounded-full object-cover 2xl:h-[76px] 2xl:w-[61px]"
       />
       <img
         src="src/assets/img/card-front-decoration.png"
         alt="golden frame decoration"
-        className="absolute top-1/2 h-[93px] w-[81px] -translate-y-2/4"
+        className="absolute top-1/2 h-[51px] w-[43px] -translate-y-2/4 2xl:h-[93px] 2xl:w-[81px]"
       />
-      <button type="button" className="absolute bottom-2">
+      <button type="button" className="absolute bottom-1 2xl:bottom-2">
         <img
           src="src/assets/img/card-front-validate.png"
           alt="golden snitch"
-          className="h-[32px] w-[32px]"
+          className="h-[17px] w-[17px] 2xl:h-[32px] 2xl:w-[32px]"
         />
       </button>
     </div>
