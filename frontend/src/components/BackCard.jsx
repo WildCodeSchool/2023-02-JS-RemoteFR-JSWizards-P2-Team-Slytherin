f99e7736b7ea911f4186dc2820af8ffd5cf250a7
BigBull69 <[slave.69@hotmail.fr]>
2023-04-18 19:02:15 +0200

    completed back card component desktop version

---
+++ frontend/src/components/BackCard.jsx
@@ -0,0 +1,5 @@
+export default function BackCard() {
+  return (
+    <div className="relative flex h-[160px] w-[112px] rounded-[6px] bg-[url('src/assets/img/card-back-background.png')] bg-cover py-2" />
+  );
+}
--- frontend/src/components/FrontCard.jsx
+++ frontend/src/components/FrontCard.jsx
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 
 export default function FrontCard({ name, image }) {
+  const firstName = name.split(" ")[0].charAt(0);
+  const lastName = name.split(" ")[1];
+  const nameShort = `${firstName}. ${lastName}`;
+
   return (
-    <div className="relative flex h-[160px] w-[112px] flex-col items-center gap-2 rounded-[6px] bg-[url('src/assets/img/card-front-background.png')] px-3.5 py-2">
-      <span className="text-caption font-baskerville">{name}</span>
+    <div className="relative flex h-[160px] w-[112px] flex-col items-center gap-2 rounded-[6px] bg-[url('src/assets/img/card-front-background.png')] bg-cover py-2">
+      <span className="font-baskerville text-sm">{nameShort}</span>
       <img
         src={image}
-        alt={name}
-        className="h-[76px] w-[61px] rounded-full object-cover"
+        alt={nameShort}
+        className="absolute top-1/2 h-[76px] w-[61px] -translate-y-2/4 rounded-full object-cover"
       />
       <img
         src="src/assets/img/card-front-decoration.png"
         alt="golden frame decoration"
-        className="absolute top-6 h-[93px] w-[81px]"
+        className="absolute top-1/2 h-[93px] w-[81px] -translate-y-2/4"
       />
       <button type="button" className="absolute bottom-2">
         <img
