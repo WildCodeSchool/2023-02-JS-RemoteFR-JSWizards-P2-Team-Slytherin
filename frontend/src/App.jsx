670873a3b77751374406a7fbf7a9952285587e21
BigBull69 <[slave.69@hotmail.fr]>
2023-04-19 15:16:58 +0200

    Adding answers to the ClueList table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import wizards from "./data/data.json";
 import FrontCard from "./components/FrontCard";
 import BackCard from "./components/BackCard";
+import ClueList from "./components/ClueList";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <FrontCard name={wizard.name} image={wizard.image} />
       <BackCard />
+      <ClueList />
     </div>
   );
 }
--- frontend/src/components/ClueList.jsx
+++ frontend/src/components/ClueList.jsx
@@ -1,3 +1,19 @@
+/* import PropTypes from "prop-types"; */
+
 export default function ClueList() {
-  return <div className="" />;
+  return (
+    <div className="flex h-[440px] w-[264px] flex-col items-center border-2 border-red-500">
+      <div className="mb-[16px] h-[60px] w-[264px] border-2 border-sky-500" />
+      <div className="mb-[16px] h-[60px] w-[264px] border-2 border-sky-500" />
+      <div className="mb-[16px] h-[60px] w-[264px] border-2 border-sky-500" />
+      <div className="mb-[16px] h-[60px] w-[264px] border-2 border-sky-500" />
+      <div className="mb-[16px] h-[60px] w-[264px] border-2 border-sky-500" />
+      <div className="h-[60px] w-[264px] border-2 border-sky-500"> </div>
+    </div>
+  );
 }
+
+/* ClueList.propTypes = {
+    : PropTypes.string.isRequired,
+    : PropTypes.string.isRequired,
+  }; */
