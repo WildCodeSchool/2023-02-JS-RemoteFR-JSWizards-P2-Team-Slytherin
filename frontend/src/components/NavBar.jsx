6c6bca525ecc0b8895c53c98e093deab2b532cb3
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-04-19 11:17:40 +0200

    final commit

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,24 +2,40 @@ const NavBar = () => {
   return (
     <nav id="navbar" class="flex justify-between items-center">
       <div>
-        <img
-          class="p-4"
-          src="src/assets/logo/logo-desktop.png"
-          alt="Return to homepage"
-        />
+        <button>
+          <a href="src/pages/Home.jsx">
+            <img
+              class="p-4"
+              src="src/assets/logo/logo-desktop.png"
+              alt="Return to homepage"
+            />
+          </a>
+        </button>
       </div>
       <ul id="menu" class="flex p-4 gap-5">
         <li>
-          <img src="src/assets/icon/rules.svg" alt="Game rules" />
+          <button>
+            <a href="src/pages/RulesPage.jsx">
+              <img src="src/assets/icon/rules.svg" alt="Game rules" />
+            </a>
+          </button>
         </li>
         <li>
-          <img src="src/assets/icon/score.svg" alt="Scores" />
+          <button>
+            <a href="src/pages/ScorePage.jsx">
+              <img src="src/assets/icon/score.svg" alt="Scores" />
+            </a>
+          </button>
         </li>
         <li>
-          <img src="src/assets/icon/sound-on.svg" alt="Sound on" />
+          <button>
+            <img src="src/assets/icon/sound-on.svg" alt="Sound on" />
+          </button>
         </li>
         <li>
-          <img src="src/assets/icon/settings.svg" alt="Settings" />
+          <button>
+            <img src="src/assets/icon/settings.svg" alt="Settings" />
+          </button>
         </li>
       </ul>
     </nav>
