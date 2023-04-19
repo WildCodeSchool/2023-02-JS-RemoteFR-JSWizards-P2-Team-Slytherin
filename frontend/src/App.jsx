ceb87045a5b452fbdfcb6e7f12d0030da8adf764
Denis Dezest <denis.dezest@gmail.com>
2023-04-19 16:38:50 +0200

    completed functional timer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,19 @@ import Home from "./pages/Home";
 import wizards from "./data/data.json";
 import FrontCard from "./components/FrontCard";
 import BackCard from "./components/BackCard";
+import Timer from "./components/Timer";
 
 import "./App.css";
 
 function App() {
   const wizard = wizards[4];
+  const gameTime = 5;
   return (
     <div className="App">
       <Home />
       <FrontCard name={wizard.name} image={wizard.image} />
       <BackCard />
+      <Timer gameTime={gameTime} />
     </div>
   );
 }
