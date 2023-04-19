a074a67e26ed8927fe105955d14a5f03651e4dd4
Enzo Castagnos <castagnos.e@gmail.com>
2023-04-19 15:31:51 +0200

    fixing tailwind parameters in TestCard.jsx

--- frontend/src/components/TestCard.jsx
+++ frontend/src/components/TestCard.jsx
@@ -2,7 +2,7 @@ import "./TestCard.css";
 
 export default function TestCard() {
   return (
-    <footer className="testcard w-32 bg-slate-500 w-28 h-40">
+    <footer className="testcard bg-slate-500 w-28 h-40">
       <h2>Card title</h2>
       <h3>Card informations</h3>
     </footer>
