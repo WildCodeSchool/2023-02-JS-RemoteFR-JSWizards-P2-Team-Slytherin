42ddd7123bf2f210c1c7a86c6dabe97a01b52cf2
Enzo Castagnos <castagnos.e@gmail.com>
2023-04-19 18:56:14 +0200

    creation of different cards in CardBoard using data file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
 import Home from "./pages/Home";
 import CardBoard from "./components/CardBoard";
 import wizards from "./data/data.json";
-import FrontCard from "./components/FrontCard";
-import BackCard from "./components/BackCard";
-
 import "./App.css";
 
 function App() {
-  const wizard = wizards[4];
+  const wizard = wizards[7];
   return (
     <div className="App">
       <Home />
-      <CardBoard />
-      <FrontCard name={wizard.name} image={wizard.image} />
-      <BackCard />
+
+      <CardBoard name={wizard.name} image={wizard.image} />
     </div>
   );
 }
