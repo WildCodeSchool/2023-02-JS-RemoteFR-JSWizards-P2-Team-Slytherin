5c66f9887cd519bc380f4f1d9a1550bf07544e0a
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-04-20 09:56:39 +0200

    create composant sorting hat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 
 import "./App.css";
+import SortingHat from "@components/SortingHat";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <SortingHat />
     </div>
   );
 }
---
+++ frontend/src/components/SortingHat.jsx
@@ -0,0 +1,10 @@
+const SortingHat = () => {
+  return (
+    <div>
+      <img src="src/assets/img/sorting-hat.png" />;
+      <h2>Hello young Wizards, welcome to Hogwart !</h2>
+    </div>
+  );
+};
+
+export default SortingHat;
