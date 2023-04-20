3bc94b845df746ba786cc7acdfe2b887136d7496
Denis Dezest <denis.dezest@gmail.com>
2023-04-20 07:55:59 +0200

    Completed static Score component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,19 @@ import Home from "./pages/Home";
 import wizards from "./data/data.json";
 import FrontCard from "./components/FrontCard";
 import BackCard from "./components/BackCard";
+import Score from "./components/Score";
 
 import "./App.css";
 
 function App() {
   const wizard = wizards[4];
+  const startingScore = 1000;
   return (
     <div className="App">
       <Home />
       <FrontCard name={wizard.name} image={wizard.image} />
       <BackCard />
+      <Score startingScore={startingScore} />
     </div>
   );
 }
