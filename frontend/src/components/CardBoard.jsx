11dee2f8148be39048df721f707b845760df9a0e
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-04-27 10:46:34 +0200

    final commmit

--- frontend/src/components/CardBoard.jsx
+++ frontend/src/components/CardBoard.jsx
@@ -5,35 +5,33 @@ import FrontCard from "./FrontCard";
 export default function CardBoard({ characters }) {
   return (
     <div>
-      <div className="flex">
-        <div className="cardcontainer flex h-[510px] w-fit flex-col items-center justify-center gap-[5px] bg-transparent text-center ">
-          <div className="content flex flex-wrap gap-[5px] ">
-            {characters.slice(0, 7).map((character) => (
-              <FrontCard
-                key={character.id}
-                name={character.name}
-                image={character.image}
-              />
-            ))}
-          </div>
-          <div className="content flex flex-wrap gap-[5px]">
-            {characters.slice(7, 13).map((character) => (
-              <FrontCard
-                key={character.id}
-                name={character.name}
-                image={character.image}
-              />
-            ))}
-          </div>
-          <div className="content flex flex-wrap gap-[5px]">
-            {characters.slice(13, 21).map((character) => (
-              <FrontCard
-                key={character.id}
-                name={character.name}
-                image={character.image}
-              />
-            ))}
-          </div>
+      <div className="cardcontainer flex h-[510px] w-fit flex-col items-center justify-center gap-[5px] bg-transparent text-center ">
+        <div className="content flex flex-wrap gap-[5px] ">
+          {characters.slice(0, 7).map((character) => (
+            <FrontCard
+              key={character.id}
+              name={character.name}
+              image={character.image}
+            />
+          ))}
+        </div>
+        <div className="content flex flex-wrap gap-[5px]">
+          {characters.slice(7, 13).map((character) => (
+            <FrontCard
+              key={character.id}
+              name={character.name}
+              image={character.image}
+            />
+          ))}
+        </div>
+        <div className="content flex flex-wrap gap-[5px]">
+          {characters.slice(13, 21).map((character) => (
+            <FrontCard
+              key={character.id}
+              name={character.name}
+              image={character.image}
+            />
+          ))}
         </div>
       </div>
     </div>
