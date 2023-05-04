9c6dbe9e4ec98a0e8b86ade208bc2e36a327d87f
Denis Dezest <126237877+ddZ6ii@users.noreply.github.com>
2023-05-04 09:34:59 +0200

    Update Timer.jsx

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -11,7 +11,8 @@ export default function Timer({
   gameDuration,
   decrementScore,
   zeroScore,
-  setIsEndGame }) {
+  setIsEndGame
+  }) {
   let timerID = null;
   const [timer, setTimer] = useState(gameDuration);
 
